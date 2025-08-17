057703aece0f933c29f01588f0377ae65fefc502
mbarbeau0545 <m.barbeau0545@gmail.com>
2025-08-17 13:20:20 +0200

    Timer Pulse working with sync and without frequency also u^dated bedore pulses, to test during generation

--- Src/FMK_HRT.h
+++ Src/FMK_HRT.h
@@ -71,6 +71,7 @@
         t_float32 frequency_f32;                 /**< Frequency of the desire PWM */
         t_eFMKHRT_LinePolarity polarity_e;      /**< Polarity of the PWM */
         t_uint32 deadTime_u32;                  /**< Dead Time between transition from Low State to High State */
+        t_bool enableSyncPulseChnlOpe_b;      /**< Enable the pulse synchronization */
     } t_sFMKHRT_PwmCfg;
 
     /**
